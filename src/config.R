4bdb0b5f7619c2156498f53401284a51133d867c
avahoffman <avamariehoffman@gmail.com>
2019-11-11 15:15:21 -0500

    Add detail to config

--- src/config.R
+++ src/config.R
@@ -1,4 +1,4 @@
-## Set working directory for the repository
+## Set working directory for the repository (should be the git repo)
 wd <- "/Users/hoffman ava/bogr_side/blue-grama-diversity/"
 
 ## Indicate whether phenotypeor phenotypic plasticity models should be saved as .R objects
