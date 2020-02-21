d3a3e0b811a2d06b3569d888a4a280fee963d7a4
Ava Hoffman <avamariehoffman@gmail.com>
2020-02-20 20:58:16 -0500

    Update runfile

--- src/run.R
+++ src/run.R
@@ -16,9 +16,12 @@ source("utils/data_utils.R")
 # Specific functions
 source("utils/mcmc_output.R")
 source("src/Trait_models.R")
+source("src/Plasticity_models.R")
 
 ###########################################################################################
 
 # Generate posterior samples for phenotypes
 run_mcmc_phenotypes()
 
+# Generate posterior samples for phenotypic plasticity
+run_mcmc_plasticity()
